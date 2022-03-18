115c04d833ca7fcc52ed6196fa31c84f3dad5572
fastlearner1110 <95926942+fastlearner1110@users.noreply.github.com>
2022-03-18 20:15:19 +0530

    Add files via upload

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,16 +4,13 @@ import InputArea from "./InputArea";
 
 function App() {
   const [items, setItems] = useState([]);
-  let [arr, setArr] = useState([]);
+  let [arr,setArr]=useState([])
 
-  function addItem(inputText, quantity, price) {
-    setArr((prevItems) => {
-      return [
-        ...prevItems,
-        { item: inputText, quantity: quantity, price: price + "/-" }
-      ];
+  function addItem(inputText,quantity,price) {
+    setArr(prevItems => {
+      return ([...prevItems,{item: inputText , quantity: quantity,price: price+"/-"}]);
     });
-  }
+   }
 
   function deleteItem(id) {
     setArr((prevItems) => {
@@ -28,21 +25,17 @@ function App() {
       <div className="heading">
         <h1>Shopping List</h1>
       </div>
-      <InputArea onAdd={addItem} />
+      <InputArea 
+      onAdd={addItem} 
+      />
       <div>
-        <div style={{ display: "flex" }}>
-          <span style={{ flex: "1", fontWeight: "900", color: "red" }}>
-            item
-          </span>
-          <span style={{ flex: "1", fontWeight: "900", color: "red" }}>
-            Quantity
-          </span>
-          <span style={{ flex: "1", fontWeight: "900", color: "red" }}>
-            Price
-          </span>
-          <span style={{ flex: "1" }}></span>
+        <div style={{display:"flex"}}>
+        <span style={{flex:"1",fontWeight:"900",color:"red"}}>item</span>
+        <span style={{flex:"1",fontWeight:"900",color:"red"}}>Quantity</span>
+        <span style={{flex:"1",fontWeight:"900",color:"red"}}>Price</span>
+        <span style={{flex:"1"}}></span>
         </div>
-
+        
         <ul>
           {arr.map((todoItem, index) => (
             <ToDoItem
--- src/components/InputArea.jsx
+++ src/components/InputArea.jsx
@@ -1,41 +1,40 @@
-import React, { useState } from "react";
+import React,{useState} from "react";
 
 function InputArea(props) {
   const [inputText, setInputText] = useState("");
-  const [quantity, setQuantity] = useState(0);
-  let [price, setPrice] = useState("");
+  const [quantity,setQuantity]=useState(0);
+  let [price,setPrice]=useState("");
   function handleChange(event) {
     let newValue = event.target.value;
     setInputText(newValue);
   }
-  let handleQuantity = (event) => {
-    let newQuantity = event.target.value;
+  let handleQuantity=(event)=>{
+    let newQuantity=event.target.value;
     setQuantity(newQuantity);
-  };
-  let handlePrice = (event) => {
-    let newPrice = event.target.value;
-    setPrice(newPrice);
-  };
+  }
+  let handlePrice=event=>{
+       let newPrice=event.target.value;
+       setPrice(newPrice);
+  }
   return (
     <div className="form">
-      <label style={{ color: "green" }}>Item</label>
-      <input onChange={handleChange} type="text" value={inputText} required />
-      <br />
-      <label style={{ color: "green" }}>Quantity</label>
-      <input onChange={handleQuantity} type="number" value={quantity} />
-      <br />
-      <label style={{ color: "green" }}>Price</label>
-      <input onChange={handlePrice} type="text" value={price} />
-      <br />
+      <label style={{color:"green"}}>Item</label>
+    <input onChange={handleChange} type="text" value={inputText} required/>
+    <br/>
+    <label style={{color:"green"}}>Quantity</label>
+    <input onChange={handleQuantity} type="number" value={quantity}/>
+    <br/>
+    <label style={{color:"green"}}>Price</label>
+    <input onChange={handlePrice} type="text" value={price}/>
+    <br/>
       <button
-        disabled={inputText.length < 1 || price.length < 1 ? true : false}
-        onClick={() => {
-          props.onAdd(inputText, quantity, price);
-          setInputText("");
-          setQuantity(0);
-          setPrice("");
-        }}
-      >
+         disabled={inputText.length<1 || price.length<1?true:false} 
+         onClick={()=>{
+         props.onAdd(inputText,quantity,price);
+         setInputText("");
+         setQuantity(0);
+         setPrice("");
+      }}>
         <span>Add</span>
       </button>
     </div>
